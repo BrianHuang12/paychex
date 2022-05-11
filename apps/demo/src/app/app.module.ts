ec91fbf2464eec061aabaac20613f08e5328f56f
Brian Huang <brianhuang12@gmail.com>
2022-05-10 19:53:52 -0700

    feat(demo): add layout for home page route
    - Add AuthGuard to home page layout with navbar

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { DEMO_ROUTES } from './routes'
 import { AppComponent } from './app.component';
+import { AuthGuard, AuthService } from '@paychex/demo-lib/auth';
+import { ShellLayoutComponentModule } from '@paychex/demo-lib/shell-layout'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(DEMO_ROUTES)],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule ,RouterModule.forRoot(DEMO_ROUTES), ShellLayoutComponentModule],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- apps/demo/src/app/routes.ts
+++ apps/demo/src/app/routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '@paychex/demo-lib/auth';
+import { ShellLayoutComponent } from '@paychex/demo-lib/shell-layout';
 
 export const DEMO_ROUTES: Routes = [
   {
+    component: ShellLayoutComponent,
+    canActivate: [AuthGuard],
     path: '',
-    pathMatch: 'full',
-    redirectTo: '/login',
   },
   {
     path: 'login',
