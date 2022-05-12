b4682b47c328b46c64f4ebd4b39a7916dc6bd03f
Brian Huang <brianhuang12@gmail.com>
2022-05-11 23:25:18 -0700

    feat(demo): add ability to start and end shifts/breaks

--- libs/demo-lib/src/lib/dashboard/dashboard.component.ts
+++ libs/demo-lib/src/lib/dashboard/dashboard.component.ts
@@ -5,7 +5,8 @@ import { MatTableModule } from '@angular/material/table'
 import { AuthGuard } from '../auth';
 import { MatButtonModule } from '@angular/material/button';
 import { DashboardService } from './dashboard.service';
-import { shareReplay } from 'rxjs';
+import { MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar'
+import { BehaviorSubject, first, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'paychex-dashboard',
@@ -13,17 +14,59 @@ import { shareReplay } from 'rxjs';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  data$ = this.dashboardService.getLoggedInUserData().pipe(shareReplay(1));
+  updateHandler$ = new BehaviorSubject<any>('');
+
+  data$ = this.updateHandler$.pipe(
+    switchMap(() => this.dashboardService.getLoggedInUserData().pipe(tap(s => console.log())))
+  );
   displayedColumns: string[] = ['createdDate', 'closedDate', 'breaks'];
 
-  constructor(private dashboardService: DashboardService) {}
+  constructor(private dashboardService: DashboardService, private snackBar: MatSnackBar) {}
 
   startShift(): void {
-
+    this.dashboardService.startShift().pipe(first()).subscribe(() => {
+      this.updateHandler$.next('');
+    }, (err) => {
+      this.snackBar.open('Cannot start a shift if one is already open.', 'Dismiss', {
+        duration: 5000,
+        panelClass: ['test']
+      });
+    })
   }
 
   startBreak(): void {
+    this.dashboardService.startBreak().pipe(first()).subscribe(() => {
+      this.updateHandler$.next('');
+    }, (err) => {
+      console.log(err)
+      this.snackBar.open('You cannot start a break if you are not working or if you already have one open.', 'Dismiss', {
+        duration: 5000,
+        panelClass: ['test']
+      });
+    })
+  }
 
+  endShift(): void {
+    this.dashboardService.endShift().pipe(first()).subscribe(() => {
+      this.updateHandler$.next('');
+    }, (err) => {
+      this.snackBar.open('Cannot end a shift if there is no open shift', 'Dismiss', {
+        duration: 5000,
+        panelClass: ['test']
+      });
+    })
+  }
+
+  endBreak(): void {
+    this.dashboardService.endBreak().pipe(first()).subscribe((test) => {
+      this.updateHandler$.next('');
+    }, (err) => {
+      console.log(err)
+      this.snackBar.open('You cannot start a break if you are not working or if you already have one open.', 'Dismiss', {
+        duration: 5000,
+        panelClass: ['test']
+      });
+    })
   }
 }
 
@@ -36,7 +79,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule, MatTableModule, MatButtonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatSnackBarModule, RouterModule.forChild(routes)],
   declarations: [DashboardComponent],
   exports: [DashboardComponent],
 })
--- libs/demo-lib/src/lib/dashboard/dashboard.service.ts
+++ libs/demo-lib/src/lib/dashboard/dashboard.service.ts
@@ -14,5 +14,21 @@ export class DashboardService {
     return this.http.get<IUser>(`http://localhost:3334/api/users/${this.authService.userId}`).pipe(tap(s => console.log(s)));
   }
 
+  startShift(): Observable<any> {
+    return this.http.post(`http://localhost:3334/api/users/${this.authService.userId}/timecharge`, {}).pipe(tap(s => console.log(s)));
+  }
+
+  endShift(): Observable<any> {
+    return this.http.patch(`http://localhost:3334/api/users/${this.authService.userId}/timecharge`, {}).pipe(tap(s => console.log(s)));
+  }
+
+  startBreak(): Observable<any> {
+    return this.http.post(`http://localhost:3334/api/users/${this.authService.userId}/break`, {}, { responseType: 'text'}).pipe(tap(s => console.log(s)));
+  }
+
+  endBreak(): Observable<any> {
+    return this.http.patch(`http://localhost:3334/api/users/${this.authService.userId}/break`, {}).pipe(tap(s => console.log(s)));
+  }
+
 
 }
